41c3614b4a3f83e6e6e031fb3339d74c6373d037
Elie AMAR <eamar@paul-f5Ar2s1.clusters.42paris.fr>
2023-07-05 23:45:10 +0200

    dissection du code : creation d'une fonction separee pour lire un fichier et remplir un map avec les donnees, on va donc pouvoir reutiliser cette fonction pour lire le deuxieme fichier

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,67 @@
 
 #include "../inc/BitcoinExchange.hpp"
 
+std::map<std::string, double>	file_to_map(std::string file, char separator)
+{
+	std::ifstream	inputFile(file.c_str());
+
+	std::map<std::string, double>	data;
+
+	std::string	line_readed	= "";
+	std::string	date		= "";
+	std::string	value_str	= "";
+	double		value		= 0;
+	size_t		pos		= 0;
+	int		loop		= 0;//remove?
+
+	std::istringstream	iss;
+
+	/*
+	   std::map<std::string, double>	database;
+	   std::string	line_readed;
+	   std::string			date;
+	   std::string			value_str;
+	   double				value;
+	   size_t	pos;
+	   std::istringstream	iss;
+	 */
+
+	while (getline(inputFile, line_readed))
+	{
+		if (loop == 0)
+		{loop++; continue;}
+
+		//security check line
+
+		pos = line_readed.find(separator);
+		//if (pos == std::string::npos) ??
+
+		date = line_readed.substr(0, pos);
+
+		//if (line_readed[pos + 1] == '\0') ??
+		value_str = line_readed.substr(pos + 1);
+
+		//std::cout << "[" << value_str << "]" << " : ";
+
+		iss.str(value_str);
+		if (loop != 0 && !(iss >> value))
+		{std::cerr << "abort: loop {" << loop << "}" << std::endl; abort();}//change
+
+		iss.clear();// est ce suffisant
+			    //iss.seekg(0); //utilite?
+
+			    //std::cout << "{" << value << "}" << std::endl;
+
+		if (data.insert(std::pair<std::string, double>(date, value)).second == false)
+			abort();
+
+	}
+
+	return	data;
+	// resoudre le probleme de la perte de la deuxieme decimale dans la conversion
+	// a partir de 2020-07-25,9551.28 (wtf)
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -15,58 +76,17 @@ int	main(int argc, char **argv)
 		return 1;
 	}
 
-	std::ifstream	inputFile(argv[1]);
-	std::string	line_readed;
-
-	std::map<std::string, float>	database;
-	std::string			date;
-	std::string			value_str;
-	float				value;
-	size_t	pos;
-
-	std::istringstream	iss;
-
 	//protect read
 
-	int	loop = 0;//remove?
+	std::map<std::string, double>	dollar_rate, bitcoins;
 
-	while (getline(inputFile, line_readed))
-	{
-		if (loop == 0)
-			{loop++; continue;}
+	dollar_rate = file_to_map("data.csv", ',');
 
-		//security check line
+	(void)argv[1];
+	//bitcoin = file_to_map(argv[1], '|')
 
-
-		pos = line_readed.find(',');
-		//if (pos == std::string::npos) ??
-
-		date = line_readed.substr(0, pos);
-
-		//if (line_readed[pos + 1] == '\0') ??
-		value_str = line_readed.substr(pos + 1);
-
-		//debug//std::cout << "[" << value_str << "]" << " : ";
-
-		iss.str(value_str);
-		if (loop != 0 && !(iss >> value))
-			{std::cerr << "abort: loop {" << loop << "}" << std::endl; abort();}//change
-
-		iss.clear();// est ce suffisant
-		//iss.seekg(0); //utilite?
-
-		//debug//std::cout << "{" << value << "}" << std::endl;
-
-		if (database.insert(std::pair<std::string, float>(date, value)).second == false)
-			abort();
-
-	}
-	// resoudre le probleme de la perte de la deuxieme decimale dans la conversion
-	// a partir de 2020-07-25,9551.28 (wtf)
-
-	for (std::map<std::string, float>::iterator it = database.begin(); it != database.end(); it++)
+	for (std::map<std::string, double>::iterator it = dollar_rate.begin(); it != dollar_rate.end(); it++)
 		std::cout << "[" << it->first << "] - [" << it->second << "]" << std::endl;
 
-	
-return 0;
+	return 0;
 }
