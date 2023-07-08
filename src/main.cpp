9f3f3fc06247b759d9e7bc1de144e2d2b9d8cd43
Elie AMAR <eamar@paul-f4Ar1s2.clusters.42paris.fr>
2023-07-08 22:05:14 +0200

    final push of day

--- src/main.cpp
+++ src/main.cpp
@@ -101,38 +101,67 @@ bool	correct_date(std::string date)
 	//TODO gerer le cas de la double date avec l'ajout des points ....	
 
 	if (date.size() != 10)
+	{ std::cout << "if (date.size() != 10)" << std::endl;
 		return false;
-	
+	}
+
 	if (!isOnlyDigits(date.substr(0,4) + date.substr(5,2) + date.substr(8,2)))
+	{ std::cout << "!Isdigitonly" << std::endl;
 		return false;
+	}
 
 	if (date[4] != '-' || date[7] != '-')
+	{ std::cout << "Guillement" << std::endl;
 		return false;
-	
+	}
+
 	std::string year_str = date.substr(0,4);
 	std::string month_str = date.substr(5,2);
 	std::string day_str = date.substr(8,2);
 
-	int	year = std::atoi(year.c_str());
-	int	month = std::atoi(month.c_str());
-	int	day = std::atoi(day.c_str());
+	int	year = std::atoi(year_str.c_str());
+	int	month = std::atoi(month_str.c_str());
+	int	day = std::atoi(day_str.c_str());
+
+	std::cout << "\n\nYOOOOOOOOOOO" << std::endl;
 
 	if (year < 2009 || year > 2023)
+	{
+	std::cout << "AAAAAAAAAA" << std::endl;
 		return false;
+	}
 
 	if (year == 2023 || month > 6)
-		return false
+	{
+	std::cout << "BBBBBBBBBBB\n";
+		return false;
+	}
 
 	if (month < 1 || month > 12)
+	{
+		std::cout << "WWWWWHHHATTT" << std::endl;
 		return false;
+	}
+
 
 	if (day < 1 || day > 31)
+	{
+		std::cout << "FFFFFFFFF" << std::endl;
 		return false;
+	}
+
 
 	if (month == 4 || month == 6 || month == 9 || month == 11)
+	{
+		std::cout <<"CCCCCCCCCCCCCCC\n";
 		if (day > 30)
+		{
+			std::cout << "day > 30" << std::endl;
 			return false;
-	
+		}
+	}
+
+
 	if (month == 2)
 	{
 		if (day > 29)
@@ -148,19 +177,20 @@ bool	correct_date(std::string date)
 			if (day > 28)
 				return false;
 	}
-		
+
 
 	return true;
 }
 
 void	check_and_calcul(std::string Bdate, double Bvalue, double Drate)
 {
+	std::cout << "----------------------------" << std::endl;
 
 	if (!correct_date(Bdate) || Bvalue < 0 || Bvalue > 1000)
 	{
 		if (!correct_date(Bdate))
 		{
-			std::cerr << "Error: bad input => " << Bdate << std::endl;
+			//std::cerr << "Error: bad input => " << Bdate << std::endl;
 			std::cerr << "INCORRECT DATE" << std::endl;
 		}
 
@@ -171,21 +201,22 @@ void	check_and_calcul(std::string Bdate, double Bvalue, double Drate)
 		return;
 	}
 
+	//(void)Drate;
 	double result = Bvalue * Drate; //check over the flow ?
 
-	std::cout << Bdate << " => " << Bvalue << " = " << result << std::endl;
+	std::cout << Bdate << " => " << Bvalue << " = " << result << "(" << Drate << ")" << std::endl;
 
 }
 
 int	main(int argc, char **argv)
 {
 	/*
-	if (argc != 2)
-	{
-		std::cerr << "Error: could not open file." << std::endl; //????????
-		return 1;
-	}
-	*/
+	   if (argc != 2)
+	   {
+	   std::cerr << "Error: could not open file." << std::endl; //????????
+	   return 1;
+	   }
+	 */
 	(void)argc;
 
 	//protect read
@@ -205,9 +236,9 @@ int	main(int argc, char **argv)
 	std::cout << "\n ----------------------- \n";
 	////////////////////////////////////////////
 	/*
-	std::cout << "correct = " << correct_date(argv[2]) << std::endl;
-	return 0;
-	*/
+	   std::cout << "correct = " << correct_date(argv[2]) << std::endl;
+	   return 0;
+	 */
 	///////////////////////////////////////////
 
 	std::map<std::string, double>::iterator itD;
@@ -224,10 +255,10 @@ int	main(int argc, char **argv)
 			{
 				if (itD_last != dollar_rate.end()) //si ce n'est pas le premier tour et donc itD_last est bon
 					check_and_calcul(itB->first, itB->second, itD->second);
-					//std::cout << "normal CROSS {" << itB->first << "} {" << itD_last->first << "}" << std::endl;
+				//std::cout << "normal CROSS {" << itB->first << "} {" << itD_last->first << "}" << std::endl;
 				else //sinon c est le premier tour et on doit prendre itD et non last
 					check_and_calcul(itB->first, itB->second, itD->second);
-					//std::cout << "first CROSS {" << itB->first << "} {" << itD->first << "}" << std::endl;
+				//std::cout << "first CROSS {" << itB->first << "} {" << itD->first << "}" << std::endl;
 
 				break;//??
 			}
@@ -236,8 +267,8 @@ int	main(int argc, char **argv)
 		}
 		//if (itD == dollar_rate.end() && !(itB->first < itD_last->first))
 		if (itD == dollar_rate.end())
-				check_and_calcul(itB->first, itB->second, itD->second);
-				//std::cout << "CROSS END {" << itB->first << "} {" << itD_last->first << "}" << std::endl;
+			check_and_calcul(itB->first, itB->second, itD->second);
+		//std::cout << "CROSS END {" << itB->first << "} {" << itD_last->first << "}" << std::endl;
 	}
 
 	std::cout << std::endl;
