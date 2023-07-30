fb6dd12c8d6f6005750402c253541d1916716c03
Elie AMAR <eamar@paul-f4Br6s7.clusters.42paris.fr>
2023-07-30 20:06:30 +0200

    adaptation de l'algorithme a la gestion des -1 qui seront tries comme des nombres normaux puis supprimes a la fin apres s etre retrouve au debut de la liste, suppression de la boucle dediee a supprimer les nombres de cote puisqu il n y en aura plus

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -124,8 +124,6 @@ void	johnson(std::vector<int> & vec, int range, char lettre)
 
 	for (int i = 0; i < middle; i++)
 	{
-		if (vec[i + middle] == -1)
-			break;
 		if ((vec[i] > vec[i + middle]))
 		{
 			std::swap(vec[i], vec[i + middle]);
@@ -143,21 +141,6 @@ void	johnson(std::vector<int> & vec, int range, char lettre)
 	johnson(vec, range / 2, lettre + 1);
 	//----------------------------------
 
-	std::vector<int>::iterator it;
-
-	if (range % 2 != 0 && range > 2) //range > 2 securite??
-	{
-		//std::cout << "          "; display(vec, range, 0, 1, 0); std::cout << "          tri du restant\n";
-		it = std::lower_bound(vec.begin(), vec.begin() + (range - 2), vec[range - 1]);
-		if (vec[range - 1] <= *it) //add//it != vec.end()
-		{
-			move_element(vec, vec.begin() + range - 1, it);
-			if (range != vec.size())
-				move_element(vec, vec.begin() + (range * 2) - 1, it + range);
-		}
-		//std::cout << "          "; display(vec, range, 0, 1, 0);
-	}
-
 	if (range == vec.size())
 	{
 		std::cout << "ENDDD\n";
@@ -169,6 +152,7 @@ void	johnson(std::vector<int> & vec, int range, char lettre)
 
 	display(vec, range, 0, 1, 0);
 
+	std::vector<int>::iterator it;
 	int j = range;
 	int loop = 0;
 
@@ -181,9 +165,6 @@ void	johnson(std::vector<int> & vec, int range, char lettre)
 		   display(vec, range, 0, 0, 0);
 		   std::cout << "J" << j << " " << "{" << vec[j] << "} " ; 
 		 */
-
-		if (vec[j] == -1)
-			return;
 
 		//lower_bound va trouver le nombre directement superieur dans la suite des nombres deja tries, ou le plus grand des nombres deja tries si vec[j] (le pounding en cours) est superieur a tous //middle - 1, -1 a tester
 		it = std::lower_bound(vec.begin(), vec.begin() + (range - 1) + loop, vec[j]);
@@ -273,10 +254,8 @@ int	main(int argc, char **argv)
 	char lettre = 'A';
 	johnson(vec, vec.size(), lettre);
 
-	std::vector<int>::iterator it = std::find(vec.begin(), vec.end(), -1);
-
-	if (it != vec.end())
-		vec.erase(it, vec.end());
+	while (*(vec.begin()) == -1)
+		vec.erase(vec.begin());
 	
 	display(vec, vec.size(), 0, 0, 0);
 
